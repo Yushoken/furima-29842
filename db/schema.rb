85c1b16f5256f697b60ff479c2c099272892f13f
yusyouken <70624366+yusyouken@users.noreply.github.com>
2020-10-05 16:15:11 +0900

    ログイン、新規登録画面作成

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 2020_10_02_052727) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "email", null: false
-    t.string "password", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.date "birth_date", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
