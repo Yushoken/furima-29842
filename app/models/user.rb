50c83e54908532501849249de8ab4fb4fe7b5464
yusyouken <70624366+yusyouken@users.noreply.github.com>
2020-10-06 19:10:10 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,22 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
+  has_many :buys
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+          #PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+          #validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers', length: {minimum: 6}     
+   validates :nickname, presence: true 
+   validates :birth_date, presence: true
+   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+     validates :first_name
+     validates :last_name
+   end
+   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width kata characters' } do
+     validates :first_name_kana
+     validates :last_name_kana
+ end
+  
+  
 end
