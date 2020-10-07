9e6271be405052507c85b08a32a4b641118c5c0c
yusyouken <70624366+yusyouken@users.noreply.github.com>
2020-10-07 17:56:15 +0900

    テストコード設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-          #PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-          #validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers', length: {minimum: 6}     
-   validates :nickname, presence: true 
-   validates :birth_date, presence: true
+  with_options presence: true do
+      validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6}+\z/, message: 'Include both letters and numbers'}     
+      validates :nickname
+      validates :birth_date
+      validates :email, format:{with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Email @ can't be expanded"} 
+  end
    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
      validates :first_name
      validates :last_name
    end
-   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width kata characters' } do
+   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width kana characters' } do
      validates :first_name_kana
      validates :last_name_kana
  end
