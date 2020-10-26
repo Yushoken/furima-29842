dc73181438fe06d9be60ab771bb4a3243fe14af4
yusyouken <70624366+yusyouken@users.noreply.github.com>
2020-10-26 13:21:36 +0900

    デバッグのコード削除

--- app/models/user_buy.rb
+++ app/models/user_buy.rb
@@ -12,7 +12,6 @@ class UserBuy
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
 
   def save
-    # binding.pry
     buy = Buy.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, buy_id: buy.id)
   end
